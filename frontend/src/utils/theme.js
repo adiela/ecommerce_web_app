6783218874b13acfb79fa2ad4fc434507b95168c
Adiela Abishua <adiela.abishua@gmail.com>
2024-03-09 15:50:19 +0300

    Add theme configuration

---
+++ frontend/src/utils/theme.js
@@ -0,0 +1,13 @@
+'use client';
+
+import { createTheme } from "@mui/material/styles";
+import { teal } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: teal,
+  },
+});
+
+export default theme;
