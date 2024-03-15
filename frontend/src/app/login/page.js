7d2b0411064cac504f0b4e79ea74e3b321c650ba
Adiela Abishua <adiela.abishua@gmail.com>
2024-03-15 18:26:31 +0300

    Edit login page

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,21 +1,89 @@
+'use client';
+
+import styles from './page.module.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ImageSideBarLayout from '@/components/ImageSideBarLayout';
+
 export default function Login() {
-    return (
-        <div>
-            <h1>login</h1>
-            <form class="form-example">
-                <fieldset class="form-example">
-                    <label for="name">Enter your name: </label>
-                    <input type="text" name="name" id="name" required />
-                </fieldset>
-                <fieldset class="form-example">
-                    <label for="email">Enter your email: </label>
-                    <input type="email" name="email" id="email" required />
-                </fieldset>
-                <fieldset class="form-example">
-                    <input type="submit" value="Subscribe!" />
-                </fieldset>
-            </form>
-        </div>
-        
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ImageSideBarLayout>
+      <Box
+          sx={{
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Login to your account
+          </Typography>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{ mt: 5 }}
+          >
+            Login with phone number
+          </Button>
+          <p class={styles.separator}>or</p>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 5 }}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email Address"
+              type="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              required
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+    </ImageSideBarLayout>   
+  );
 }
