7da0340333164653ca24d961c814d726996fcb3c
Adiela Abishua <adiela.abishua@gmail.com>
2024-03-15 18:23:05 +0300

    Edit mui theme components

--- frontend/src/utils/theme.js
+++ frontend/src/utils/theme.js
@@ -2,12 +2,45 @@
 
 import { createTheme } from "@mui/material/styles";
 import { teal } from '@mui/material/colors';
+import { Roboto } from "next/font/google";
 
 const theme = createTheme({
   palette: {
     primary: teal,
     secondary: teal,
   },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: 'contained',
+        size: 'large'
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: 20,
+          boxShadow: '0px 4px 6px #e0e0e0'
+        },
+    },
+    },
+    MuiTextField: {
+      defaultProps: {
+        variant: 'filled',
+        fullWidth: true,
+        margin: 'normal',
+        InputProps: {
+          disableUnderline: true,
+          style: {
+            borderRadius: 20,
+          }
+        },
+      },
+    },
+    MuiCheckbox: {
+      defaultProps: {
+        variant: 'filled'
+      }
+    }
+  }
 });
 
 export default theme;
